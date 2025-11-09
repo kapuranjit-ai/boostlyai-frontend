7f135e067cceca3e2b4d90d8a8122617ec494867
kapuranjit-ai <kapuranjit@gmail.com>
2025-11-09 16:00:08 +0530

    add backend api base url change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
-
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+//localhost
+//const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://boostlyai-backend.onrender.com/';
 
 // Create axios instance
 const api = axios.create({
